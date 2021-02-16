f5c7066620342ba4963f70126342e35b08552db4
Wesley <wspencer3295@yahoo.com>
2021-02-15 17:20:21 -0700

    commit tree 5ed9a41b8ee89d82eb3743c1a21bf18c02164478

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let tex = document.querySelector("#text");
+    return tex.textContent = 'This is really cool!'
+  }); 
+
+ 
